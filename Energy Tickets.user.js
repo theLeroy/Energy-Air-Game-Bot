91bbc425ff5871f6af9d96b2b48d1151df0c4a84
Unknown <tom.diggelmann@swisscom.com>
2017-08-24 16:06:47 +0200

    Energy Ticket state
    - makes the ticket state "verschenkt" possible

---
+++ Energy Tickets.user.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name         Energy Tickets
+// @namespace    https://github.com/Tomzilla12/Energy-Air-Game-Bot
+// @version      1.0
+// @description  Color the Energy Tickets when clicked
+// @author       Tomzilla12: https://github.com/Tomzilla12
+// @match        *energy.ch/*
+// @run-at       document-idle
+// @grant        none
+// @updateURL    
+// @downloadURL  
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js
+// ==/UserScript==
+
+function setColor(coloredTicketIds) {
+    $('.energy-ticket-info a').parent().css('background-color','#FFFFFF');
+    $('.energy-ticket-info .energy-ticket-state').html('geöffnet');
+    coloredTicketIds.forEach(function(ticketId) {
+        $('.energy-ticket-info a[href="' + ticketId + '"]').parent().css('background-color','#D6A49F');
+        $('.energy-ticket-info a[href="' + ticketId + '"]').parent().find('.energy-ticket-state').html('verschenkt');
+    });
+}
+
+(function() {
+    'use strict';
+    
+    $('body').keypress(function(event) {
+        $('.energy-ticket-info').css('cursor','pointer');
+
+        var coloredTicketIds;
+        if (localStorage.getItem("coloredTickets") !== null) {
+            coloredTicketIds = JSON.parse(localStorage.getItem('coloredTickets'));
+        } else {
+            coloredTicketIds = [];
+        }
+        console.log(coloredTicketIds);
+
+        $('.energy-ticket-info').click(function() {
+            var ticketId = $(this).find('a').attr('href');
+            var coloredTicketIds = JSON.parse(localStorage.getItem('coloredTickets'));
+
+            if (localStorage.getItem("coloredTickets") !== null) {
+                var alreadyIncluded = false;
+                coloredTicketIds.forEach(function(element) {
+                    if (element == ticketId) {
+                        alreadyIncluded = true;
+                    }
+                });
+                if (!alreadyIncluded) {
+                    coloredTicketIds.push(ticketId);
+                } else {
+                    var index = coloredTicketIds.indexOf(ticketId);
+                    if (index > -1) {
+                        coloredTicketIds.splice(index, 1);
+                    }
+                }
+            } else {
+                coloredTicketIds = [ticketId];
+            }
+            
+            localStorage.setItem('coloredTickets', JSON.stringify(coloredTicketIds));
+            console.log(coloredTicketIds);
+
+            setColor(JSON.parse(localStorage.getItem('coloredTickets')));
+        });
+        setColor(JSON.parse(localStorage.getItem('coloredTickets')));
+    });
+})();
